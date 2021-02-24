01629e8c79dc3c386ac42d1564a5a72846f264fa
Mastrianth <kholodnyack.n@yandex.ua>
2021-02-24 19:08:47 +0300

    Updates

---
+++ precache-manifest.58afa0c7ad99413af2ad92934210afe8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf33d750c63a8e70b40d3cffd5f4ba2",
+    "url": "/tt/index.html"
+  },
+  {
+    "revision": "009340127a6ded8425f0",
+    "url": "/tt/static/css/main.ef086b1f.chunk.css"
+  },
+  {
+    "revision": "0267cb15755dbda7cd2e",
+    "url": "/tt/static/js/2.7d69b6c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tt/static/js/2.7d69b6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "009340127a6ded8425f0",
+    "url": "/tt/static/js/main.1c4ab176.chunk.js"
+  },
+  {
+    "revision": "0e40981f5a16ae755310",
+    "url": "/tt/static/js/runtime-main.18c2270b.js"
+  },
+  {
+    "revision": "69e99028fb3aff0756cb911ac9efcef3",
+    "url": "/tt/static/media/banner-photo1024.69e99028.jpg"
+  },
+  {
+    "revision": "9543dfde02b1e508f418a0a0a7be1b7d",
+    "url": "/tt/static/media/banner-photo1024@2x.9543dfde.jpg"
+  },
+  {
+    "revision": "cd0265bd00c62e8cd5ec94bbc771d70b",
+    "url": "/tt/static/media/banner-photo360.cd0265bd.jpg"
+  },
+  {
+    "revision": "fdb437e6f8d7e4f91d539de26d3beae1",
+    "url": "/tt/static/media/banner-photo360@2x.fdb437e6.jpg"
+  },
+  {
+    "revision": "9233c4b3da6d6061474aa45d1ad6c8af",
+    "url": "/tt/static/media/banner-photo526.9233c4b3.jpg"
+  },
+  {
+    "revision": "8ab047c6b1b3fd8e2c7decfe18562934",
+    "url": "/tt/static/media/banner-photo526@2x.8ab047c6.jpg"
+  },
+  {
+    "revision": "9511718cbb72d542e5ee9b7ea28a991b",
+    "url": "/tt/static/media/banner-photo768.9511718c.jpg"
+  },
+  {
+    "revision": "6088de7485e8367fdba9bc73b03c9920",
+    "url": "/tt/static/media/banner-photo768@2x.6088de74.jpg"
+  },
+  {
+    "revision": "61316c5cc17986c521c0815070a08053",
+    "url": "/tt/static/media/close_icon.61316c5c.svg"
+  },
+  {
+    "revision": "bce28fb772838de34344b20fa26296b0",
+    "url": "/tt/static/media/logo.bce28fb7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/tt/static/media/man-laptop-v1.f28cb247.svg"
+  },
+  {
+    "revision": "63637e948daaff9637f3f5a803d2b669",
+    "url": "/tt/static/media/menu icon.63637e94.svg"
+  }
+]);
